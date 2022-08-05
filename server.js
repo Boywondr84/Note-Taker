0f15520b75892718d043e33049397c85a42d0904
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-05 13:55:14 -0500

    fixed issue with index.js and style.css not loading

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
