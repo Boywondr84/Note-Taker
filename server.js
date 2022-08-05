86fe9fd8e6a5bd0cf83e04e6ab555f5df8e8b4b0
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-05 02:22:15 -0500

    able to render index.html, notes.html and db notes pages

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const path = require("path");
 const PORT = process.env.PORT || 3002;
 const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const { notes } = require('./Develop/db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log("API server now on port ${PORT}");
